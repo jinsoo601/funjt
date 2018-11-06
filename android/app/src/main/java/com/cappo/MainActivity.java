1e13eb6d3a88072f46a208182cf92d025bfa8a5a
JinsooLim <jinsoo@Jinsoo-MacBook-Pro.local>
2018-11-06 00:07:30 -0500

    react native navigation android setup

--- android/app/src/main/java/com/cappo/MainActivity.java
+++ android/app/src/main/java/com/cappo/MainActivity.java
@@ -1,15 +1,6 @@
 package com.cappo;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "cappo";
-    }
+public class MainActivity extends NavigationActivity {
 }
